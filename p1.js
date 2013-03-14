7ce87001f0477770ad99686c2276101d32d97949
snow1987 <everybodygoes@nate.com>
2013-03-14 22:27:18 +0900

    project1

---
+++ p1.js
@@ -0,0 +1,13 @@
+var a = ""
+for(var y = 0; y < 5; y++){
+	a = a + "*";
+	console.log(a);
+	}
+
+for(y = y - 1; y > 0; y--){
+	a = "";
+	for(var x = 0; x < y; x++){
+	a = a + "*";
+	}
+	console.log(a);
+	}
