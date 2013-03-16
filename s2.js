8d466c68e489b199fd7508c7cc91537f5fc3571b
snow1987 <everybodygoes@nate.com>
2013-03-17 02:11:33 +0900

    success1

--- s2.js
+++ s2.js
@@ -15,13 +15,13 @@ for(var z = 0; z < 3; z++) {
 	c = '';
 	var d = 5 - 2 * z;
 	for(var x = 0; x < z; x++) {
-		a = a + "#";
+		a = a + " ";
 		}
 	for(x = 0; x < d; x++) {
 		b = b + "*";
 		}
 	for(x = 0; x < z; x++) {
-		c = c + "#"
+		c = c + " "
 		}
 	d = a + b + c;
 	console.log(d);
@@ -32,16 +32,17 @@ for (var q = 1; q < 3; q++) {
 	b = '';
 	c = '';
 	var e = 2 * q + 1;
-	for(var y = 2; y > z; y--) {
-		a = a + '#';
+	for(var y = 2; y > q; y--) {
+		a = a + " ";
 		}
 	for(var u = 0; u < e; u++) { //e = 3, 5
 		b = b + "*";
+		
 		}
-	for(y = 2; y > z; y--) {
-		c = c + "#";
+	for(y = 2; y > q; y--) {
+		c = c + " ";
 		}
 	f = a + b + c;
-	console.log(d);
+	console.log(f);
 	}
-	
+		
